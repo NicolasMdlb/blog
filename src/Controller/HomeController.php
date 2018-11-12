ae637c427af082ca94139ecde8b25f718b5cda4a
Nicolas de LA BAUME <nicolas.mdlb@gmail.com>
2018-11-12 15:26:24 +0100

    home

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mcnitch
+ * Date: 12/11/18
+ * Time: 13:46
+ */
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/home/home")
+     */
+    public function index()
+    {
+        return $this->render('home/home.html.twig');
+    }
+}
