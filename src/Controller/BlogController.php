f58f3b0f8287202e8a616a50282819941a97f337
Nicolas de LA BAUME <nicolas.mdlb@gmail.com>
2018-11-15 15:21:22 +0100

    liberation

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -10,6 +10,7 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Entity\Article;
+use App\Repository\CategoryRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,20 +18,12 @@ use Symfony\Component\HttpFoundation\Response;
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/blog", name="blog_list")
-     */
-    public function list()
-    {
-        return $this->render('blog/index.html.twig');
-    }
-
-    /**
      * Show all row from article's entity
      *
-     * @Route("/", name="blog_index")
+     * @Route("/blog", name="blog_index")
      * @return Response A response instance
      */
-    public function index() : Response
+    public function index(): Response
     {
         $articles = $this->getDoctrine()
             ->getRepository(Article::class)
@@ -53,12 +46,12 @@ class BlogController extends AbstractController
      *
      * @param string $slug The slugger
      *
-     * @Route("/{slug<^[a-z0-9-]+$>}",
+     * @Route("/blog/{slug<^[a-z0-9-]+$>}",
      *     defaults={"slug" = null},
      *     name="blog_show")
-     *  @return Response A response instance
+     * @return Response A response instance
      */
-    public function show($slug) : Response
+    public function show($slug): Response
     {
         if (!$slug) {
             throw $this
@@ -66,8 +59,8 @@ class BlogController extends AbstractController
         }
 
         $slug = preg_replace('/-/',
-                            ' ', ucwords(trim(strip_tags($slug)),
-                            "-")
+            ' ', ucwords(trim(strip_tags($slug)),
+                "-")
         );
 
         $article = $this->getDoctrine()
@@ -76,7 +69,7 @@ class BlogController extends AbstractController
 
         if (!$article) {
             throw $this->createNotFoundException(
-                'No article with '.$slug.' title, found in article\'s table.'
+                'No article with ' . $slug . ' title, found in article\'s table.'
             );
         }
 
@@ -84,24 +77,43 @@ class BlogController extends AbstractController
             'blog/show.html.twig',
             [
                 'article' => $article,
-                'slug' => $slug,
+                'slug' => $slug
             ]
         );
     }
 
     /**
-     * @Route("/category/{category}", name="category")
+     * @Route("/blog/category/{categoryName}/all", name="blog_showcat")
      * @return Response A response instance
      */
-    public function showByCategory(String $category)
+    public function showAllByCategory($categoryName) : Response
     {
         $category = $this->getDoctrine()
             ->getRepository(Category::class)
-            -> findOneByName($category);
+            ->findOneByName($categoryName);
+
+        $articles = $category->getArticles();
+
+        return $this->render(
+            'blog/showcat.html.twig',
+            ['articles' => $articles,
+                'category' => $category]
+        );
+    }
+
+    /**
+     * @Route("/blog/category/{categoryName}", name="category")
+     * @return Response A response instance
+     */
+    public function showByCategory(String $categoryName)
+    {
+        $category = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findOneByName($categoryName);
 
         $articles = $this->getDoctrine()
             ->getRepository(Article::class)
-            -> findBy(['category' => $category], ['id' => 'DESC'], 3);
+            ->findBy(['category' => $category], ['id' => 'DESC'], 3);
 
         return $this->render(
             'blog/category.html.twig',
--- src/Migrations/Version20181115094224.php
+++ src/Migrations/Version20181115094224.php
@@ -8,14 +8,14 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20181113100727 extends AbstractMigration
+final class Version20181115094224 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE category CHANGE name name VARCHAR(100) NOT NULL');
+        $this->addSql('ALTER TABLE article CHANGE category_id category_id INT NOT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -23,6 +23,6 @@ final class Version20181113100727 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE category CHANGE name name VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci');
+        $this->addSql('ALTER TABLE article CHANGE category_id category_id INT DEFAULT NULL');
     }
 }
