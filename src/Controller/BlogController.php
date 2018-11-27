9f9aaf78e8ad5250dcf6d77087c98d20cba00b6d
Nicolas de LA BAUME <nicolas.mdlb@gmail.com>
2018-11-27 13:46:17 +0100

    refactorisation show article

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -39,7 +39,7 @@ class BlogController extends AbstractController
             );
         }
 
-             $article = new article;
+        $article = new article;
         $form = $this->createForm(articleType::class, $article, ['method' => Request::METHOD_GET])
             ->add('title', TextType::class)
             ->add('content', TextType::class);
@@ -70,27 +70,13 @@ class BlogController extends AbstractController
      *     name="blog_show")
      * @return Response A response instance
      */
-    public function show($slug): Response
+    public function show(Article $article): Response
     {
-        if (!$slug) {
-            throw $this
-                ->createNotFoundException('No slug has been sent to find an article in article\'s table.');
-        }
-
-        $slug = preg_replace('/-/',
-            ' ', ucwords(trim(strip_tags($slug)),
-                "-")
-        );
-
-        $article = $this->getDoctrine()
-            ->getRepository(Article::class)
-            ->findOneBy(['title' => mb_strtolower($slug)]);
-
         $tags = $article->getTags();
 
         if (!$article) {
             throw $this->createNotFoundException(
-                'No article with ' . $slug . ' title, found in article\'s table.'
+                'No article with ' . $article . ' title, found in article\'s table.'
             );
         }
 
@@ -98,7 +84,6 @@ class BlogController extends AbstractController
             'blog/show.html.twig',
             [
                 'article' => $article,
-                'slug' => $slug,
                 'tags' => $tags
             ]
         );
